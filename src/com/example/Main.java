ac172f4198b2e1c8ea1ecb84c06629726695fa68
adrienne <adriennebing@gmail.com>
2017-07-25 22:48:16 -0400

    upload basic project requirements

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -9,24 +9,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-
-        System.out.println("Please select a starting month : ");
+        
+        System.out.println("Please enter the name of a starting month : ");
         for (Month eachMonth : Month.values()){
             System.out.println(eachMonth);
         }
 
         Scanner scanner = new Scanner(System.in);
-        String userSelectedMonth = scanner.nextLine();
+        String monthEntered = scanner.nextLine();
+        String userSelectedMonth;
+        userSelectedMonth = monthEntered.toUpperCase();
+        System.out.println("The month you entered was: " + userSelectedMonth);
 
-        List<String> monthArrayList = new ArrayList<>();
-        monthArrayList.add(userSelectedMonth);
-//        int index = 1;
+        List<Month> monthArrayList = new ArrayList<>();
+        int index = 0;
+
         for(Month eachMonth : Month.values()){
-            if(userSelectedMonth.equals()){
-
-            }
-//            monthArrayList.add();
+           if(eachMonth.toString().equals(userSelectedMonth)){
+               index = eachMonth.ordinal();
+           }
         }
+
+        for(int i = index;i<Month.values().length;i++){
+            monthArrayList.add(Month.values()[i]);
+        }
+
+        System.out.println("Your months are as follows: "+ monthArrayList);
+        System.out.println(monthArrayList);
     }
 }
